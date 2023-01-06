2198dd0b4d58d757a37f1e848d55b23e8161e397
Tiseno <mathias.lindgren@stabelo.se>
2023-01-06 06:56:11 +0100

    Love 2d boiler plate.

---
+++ main.lua
@@ -0,0 +1,14 @@
+local love = require("love")
+
+function love.load(dt)
+  print("Loaded")
+end
+
+function love.update(dt)
+  print("Updated")
+end
+
+function love.draw(dt)
+  print("Drawed")
+end
+
