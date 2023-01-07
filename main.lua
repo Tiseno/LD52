0bbaac59912d38710473e09e41f2af751017e256
Tiseno <mathias.lindgren@stabelo.se>
2023-01-07 12:59:07 +0100

    Bendy wheat.

--- main.lua
+++ main.lua
@@ -1,11 +1,18 @@
 local love = require("love")
 
-local MAIN_MENU = 0
-local GAME_RUNNING = 1
-local GAME_PAUSED = 2
-local GAME_OVER = 3
-local STATE = MAIN_MENU
+local RED = {1, 0, 0}
+local GREEN = {0, 1, 0}
+local BLUE = {0, 0, 1}
 
+local MAIN_MENU = "MAIN_MENU"
+local GAME_INIT = "GAME_INIT"
+local GAME_RUNNING = "GAME_RUNNING"
+local GAME_PAUSED = "GAME_PAUSED"
+local GAME_OVER = "GAME_OVER"
+
+local STATE = GAME_INIT
+
+CyclicTime = 0
 Time = 0
 
 function love.keypressed(key, scancode, isrepeat)
@@ -26,6 +33,7 @@ function love.keypressed(key, scancode, isrepeat)
     end
 end
 
+-- SkyVaultGames: https://www.youtube.com/watch?v=vMSjVuJ6wDs
 local function newMenuButton(text, fn)
     return {
         text = text,
@@ -37,6 +45,43 @@ end
 local main_menu = {}
 local pause_menu = {}
 
+local function newBone(x, y, width, height, angle, color, children)
+    return {
+        x = x,
+        y = y,
+        width = width,
+        height = height,
+        angle = angle,
+        color = color,
+        children = children
+    }
+end
+
+local function newWheat(x, y)
+    local color = {200 / 255, 150 / 255, 100 / 255}
+
+    -- 16 total, 7 on left, 8 on right, 1 at the top
+    local seeds = {}
+    for i = 0, 7, 1 do
+        table.insert(seeds, newBone(0, i * 6, 3, 10, 0.4, color, {}))
+    end
+    for i = 0, 8, 1 do
+        table.insert(seeds, newBone(0, i * 6 - 3, 3, 10, -0.4, color, {}))
+    end
+    table.insert(seeds, newBone(0, 8 * 6, 3, 10, 0, color, {}))
+
+    local seed = math.random() * math.pi
+    local bend = -0.09 + math.random() * 0.18
+    local random_height = math.random() * 50
+    local stalk3 = newBone(0, 0, 2, 30 + random_height, bend, color, seeds)
+    local stalk2 = newBone(0, 0, 2, 30 + random_height, bend, color, {stalk3})
+    local stalk1 = newBone(0, 0, 2, 30 + random_height, bend + math.pi * 1 + math.random() * 0.08, color, {stalk2})
+
+    return {x = x, y = y, skeleton = stalk1, bend = bend, seed = seed}
+end
+
+local wheats = {}
+-- https://love2d.org/wiki/Tutorial:Physics
 local function initGameWorld()
     World = love.physics.newWorld(0, 981, true)
     Objects = {}
@@ -47,11 +92,16 @@ local function initGameWorld()
     Objects.ground.body = love.physics.newBody(World, window_width / 2, 520)
     Objects.ground.shape = love.physics.newRectangleShape(window_width, 40)
     Objects.ground.fixture = love.physics.newFixture(Objects.ground.body, Objects.ground.shape)
+
+    for i = 0, 200, 1 do
+        table.insert(wheats, newWheat(0 + i * 5 + math.random()*3, 500))
+    end
 end
 
 local function destroyWorld()
     Objects = {}
     World = {}
+    wheats = {}
 end
 
 function love.load()
@@ -63,8 +113,8 @@ function love.load()
         newMenuButton(
             "Start Game",
             function()
-                initGameWorld()
-                STATE = GAME_RUNNING
+                print("Changed state to init")
+                STATE = GAME_INIT
             end
         )
     )
@@ -101,12 +151,27 @@ function love.load()
     print("Loaded")
 end
 
+function updateWheat(wheat)
+
+end
+
 function love.update(dt)
+    if STATE == GAME_INIT then
+        initGameWorld()
+        STATE = GAME_RUNNING
+    end
+
     if STATE ~= GAME_RUNNING then
         return
     end
+
+    CyclicTime = CyclicTime + dt
+    if CyclicTime > math.pi then
+        CyclicTime = CyclicTime - math.pi
+    end
+
     Time = Time + dt
-    if Time > 1 then
+    if Time > math.pi then
         print(Objects.ground.body:getX(), Objects.ground.body:getY())
         Time = Time - 1
     end
@@ -127,6 +192,10 @@ function love.update(dt)
 
     if love.keyboard.isDown("right") then
         Objects.ground.body:setX(Objects.ground.body:getX() + dt * 100)
+    end
+
+    for i, wheat in ipairs(wheats) do
+        updateWheat(wheat)
     end
 end
 
@@ -161,17 +230,79 @@ local function drawMenu(menu)
     end
 end
 
+-- https://love2d.org/wiki/love.graphics.rectangle
+local function drawRotatedRectangle(x, y, width, height, angle, color)
+    -- We cannot rotate the rectangle directly, but we
+    -- can move and rotate the coordinate system.
+    love.graphics.push()
+    love.graphics.translate(x, y)
+    love.graphics.rotate(angle)
+    -- love.graphics.rectangle(mode, 0, 0, width, height) -- origin in the top left corner
+    love.graphics.setColor(unpack(color))
+    love.graphics.rectangle("fill", -width / 2, -height / 2, width, height) -- origin in the middle
+    love.graphics.pop()
+end
+
+local function drawBone(bone)
+    love.graphics.push()
+    love.graphics.translate(bone.x, bone.y)
+    love.graphics.rotate(bone.angle)
+    love.graphics.setColor(unpack(bone.color))
+    love.graphics.rectangle("fill", -bone.width / 2, 0, bone.width, bone.height) -- origin in the middle bottom
+    -- love.graphics.rectangle("fill", 0, 0, bone.width, bone.height) -- origin in the top left corner
+    -- love.graphics.rectangle("fill", -bone.width / 2, -bone.height / 2, bone.width, bone.height) -- origin in the middle
+    love.graphics.translate(0, bone.height)
+    for i, childBone in ipairs(bone.children) do
+        drawBone(childBone)
+    end
+    love.graphics.pop()
+end
+
+local function drawSkeleton(x, y, skeleton)
+    love.graphics.push()
+    love.graphics.translate(x, y)
+    drawBone(skeleton)
+    love.graphics.pop()
+end
+
+
+local function drawWheat(w)
+    drawSkeleton(w.x, w.y, w.skeleton)
+end
+
+local function drawPoint(x, y, size, color)
+    love.graphics.setColor(unpack(color))
+    love.graphics.rectangle("fill", x - (size / 2), y - (size / 2), size, size)
+end
+
+local function drawWorld()
+    love.graphics.setColor(0.5, 0.7, 0.9)
+    love.graphics.polygon("fill", Objects.ground.body:getWorldPoints(Objects.ground.shape:getPoints()))
+
+    for i, wheat in ipairs(wheats) do
+        drawWheat(wheat)
+    end
+end
+
 function love.draw(dt)
+    print(STATE)
+
     if STATE == MAIN_MENU then
         drawMenu(main_menu)
-    else
-        love.graphics.setColor(0.5, 0.7, 0.9)
-        love.graphics.polygon("fill", Objects.ground.body:getWorldPoints(Objects.ground.shape:getPoints()))
-        if STATE == GAME_PAUSED then
-            drawMenu(pause_menu)
-        end
-        if STATE == GAME_OVER then
-        -- TODO
-        end
+    end
+
+    if STATE == GAME_RUNNING then
+        drawWorld()
+    end
+
+    if STATE == GAME_PAUSED then
+        drawWorld()
+        -- TODO draw shadow
+        drawMenu(pause_menu)
+    end
+
+    if STATE == GAME_OVER then
+        drawWorld()
+    -- TODO
     end
 end
